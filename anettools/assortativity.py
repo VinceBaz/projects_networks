81bec24a98cd839653e2071facdb9e96829e0b83
vincent bazinet <vincent.bazinet@hotmail.com>
2019-11-24 14:05:06 -0500

    [TEST] Check for dimension of inputs in getPage...

--- anettools/assortativity.py
+++ anettools/assortativity.py
@@ -296,7 +296,11 @@ def getPageRankWeights(A, degree, i, pr, n, maxIter=1000):
                [Multiscale PageRank]
     it      -> Number of iteration
     '''
-
+	
+    #Check if dimension of inputs are valid
+	if degree.ndim != 1 or A.ndim != 2:
+		raise TypeError("Dimensions of A and degree must respectively be 2 and 1")
+	
     # Divide each row 'i' by the degree of node 'i', then get the transpose
     W = A/degree[:, np.newaxis]
     W = W.T  # Gives you the Markov Matrix of the network
