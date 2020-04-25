44fef1a12f849b94dc1ed73dfdbc6b51f1f202cf
Vincent Bazinet <vincent.bazinet@mail.mcgill.ca>
2020-04-25 11:45:00 -0400

    [DOC] Updated docstring documentation

--- projects_networks/diffusion.py
+++ projects_networks/diffusion.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+
+Functions that are useful for investigating the diffusive architecture of
+networks.
+
 Created on : 2020/03/13
 Last updated on: 2020/03/13
 @author: Vincent Bazinet
@@ -10,17 +14,32 @@ import numpy as np
 
 def getPageRankWeights(A, i, pr, maxIter=1000):
     '''
-    Function that gives you the personalized pagerank of node i in network A
-    INPUTS:
-    A       -> Adjacency matrix representation of your network. Dtype: (n, n)
-               ndarray where n is the number of nodes in the network
-    i       -> Index of node of interest
-    pr      -> Probability of restart
-    OUTPUTS:
-    F 		-> The Pagerank weights for each node
-    T       -> The integral scores of a distribution of pagerank weights
-               [Multiscale PageRank]
-    it      -> Number of iteration
+    Function that gives you the personalized PageRank of a node in a network
+
+    Parameters
+    ----------
+    A : ndarray (n,n)
+        Adjacency matrix representation of the network of interest, where
+        n is the number of nodes in the network.
+    i : int
+        Index of the node of interest.
+    pr : float
+        Damping factor (One minus the probability of restart). Must be between
+        0 and 1.
+    maxIter : int
+        Number of maximum of iterations to perform in case the algorithm
+        does not converge (if reached, then prints a warning)
+
+    Returns
+    -------
+    F : ndarray (n,)
+        The personalized PageRank vector of node i, for the selected damping
+        factor
+    T : ndarray (n,)
+        The integral scores over the PageRank vector scores obtained for
+        values of the damping factor. Also called Multiscale PageRank.
+    it : int
+        Number of iterations
     '''
 
     degree = np.sum(A, axis=1)  # out-degrees
@@ -30,31 +49,35 @@ def getPageRankWeights(A, i, pr, maxIter=1000):
     if degree.ndim != 1 or A.ndim != 2:
         raise TypeError("Dimensions of A and degree must be 2 and 1")
 
-    # Divide each row 'i' by the degree of node 'i', then get the transpose
+    # Compute the Transition matrix (transposed) of the network
     W = A/degree[:, np.newaxis]
-    W = W.T  # Gives you the Markov Matrix of the network
+    W = W.T
 
     # Initialize parameters...
-    # F      -> The PageRank weights (current)
-    # Fold   -> The PageRank weights (old)
     diff = 1
     it = 1
-    F = np.zeros(n)
+    F = np.zeros(n)  # PageRank (current)
     F[i] = 1
-    Fold = F.copy()
-    T = F.copy()
+    Fold = F.copy()  # PageRank (old)
+    T = F.copy()     # Pagerank [multiscale]
     oneminuspr = 1-pr
 
     # Start Power Iteration...
     while diff > 1e-9:
+
+        # Compute next PageRank
         F = pr*W.dot(F)
         F[i] += oneminuspr
+
+        # Compute multiscale PageRank
         T += (F-Fold)/((it+1)*(pr**it))
-        diff = np.sum((F-Fold)**2)
+
         it += 1
         if it > maxIter:
             print(i, "max iterations exceeded")
             diff = 0
+
+        diff = np.sum((F-Fold)**2)
         Fold = F.copy()
 
     return F, T, it
